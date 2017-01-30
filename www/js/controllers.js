f6e33ad60296a3c22b05e7c7d87e53530ff333d6
DTTEC\jmoren16 <jmoren16@tragsa.es>
2017-01-30 16:31:16 +0100

    Added new categories

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -63,7 +63,7 @@ angular.module('app.controllers', [])
         // You can include any angular dependencies as parameters for this function
         // TIP: Access Route Parameters for your page via $stateParams.parameterName
         function ($scope, $stateParams, sessionService) {
-            sessionService.greeter();
+            //console.log(sessionService.get("test"));
         }])
 
     .controller('countdownCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
