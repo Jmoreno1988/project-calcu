7215feb2de4de5ce61444b9bb30368a06e158ec7
DTTEC\jmoren16 <jmoren16@tragsa.es>
2017-03-16 16:15:58 +0100

    qwerty

--- www/js/controllers/resultCtrl.js
+++ www/js/controllers/resultCtrl.js
@@ -1,36 +1,40 @@
-appControllers.controller('resultCtrl', ['$scope', '$stateParams', 'bridgeService', '$ionicSideMenuDelegate',
-        function ($scope, $stateParams, bridgeService, $ionicSideMenuDelegate) {
-                var listScores = bridgeService.data.listScore;
-                var totalScore = listScores.reduce(function (a, b) { return a + b; }, 0);
-                var timeTotal = bridgeService.data.timeCal;
-                var level = bridgeService.data.levelCal;
-                var selectLevel = bridgeService.data.selectLevel;
-                var levelWin = bridgeService.data.levelCalWin;
-                var isWin = level >= levelWin ? true : false;
-                var msgResult = isWin ? "You win!!" : "You lose";
+appControllers.controller('resultCtrl', ['$scope', '$stateParams', 'bridgeService', '$ionicSideMenuDelegate', '$state',
+    function ($scope, $stateParams, bridgeService, $ionicSideMenuDelegate, $state) {
+        var listScores = bridgeService.data.listScore;
+        var totalScore = listScores.reduce(function (a, b) { return a + b; }, 0);
+        var timeTotal = bridgeService.data.timeCal;
+        var level = bridgeService.data.levelCal;
+        var selectLevel = bridgeService.data.selectLevel;
+        var levelWin = bridgeService.data.levelCalWin;
+        var isWin = level >= levelWin ? true : false;
+        var msgResult = isWin ? "You win!!" : "You lose";
 
-                $scope.isWin = msgResult;
-                $scope.level = level;
-                $scope.levelWin = levelWin;
-                $scope.timeTotal = timeTotal;
-                $scope.totalScore = totalScore;
-                $scope.selectLevel = selectLevel;
+        $scope.isWin = msgResult;
+        $scope.level = level;
+        $scope.levelWin = levelWin;
+        $scope.timeTotal = timeTotal;
+        $scope.totalScore = totalScore;
+        $scope.selectLevel = selectLevel;
 
-                $scope.openSettings = function () { $ionicSideMenuDelegate.toggleLeft() }
+        $scope.openSettings = function () { $ionicSideMenuDelegate.toggleLeft() }
 
-                // Graficos
-                var labels = [];
+        // Graficos
+        var labels = [];
 
-                for (var i = 0; i < listScores.length; i++)
-                        labels.push("L" + (i + 1));
+        for (var i = 0; i < listScores.length; i++)
+            labels.push("L" + (i + 1));
 
-                new Chartist.Line('.ct-chart', {
-                        labels: labels,
-                        series: [listScores]
-                }, {
-                                fullWidth: true,
-                                chartPadding: {
-                                        right: 40
-                                }
-                        });
-        }])
+        new Chartist.Line('.ct-chart', {
+            labels: labels,
+            series: [listScores]
+        }, {
+                fullWidth: true,
+                chartPadding: {
+                    right: 40
+                }
+            });
+
+        $scope.goTo = function (page) {
+            $state.go(page);
+        }
+    }])
