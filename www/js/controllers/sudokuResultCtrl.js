12460c4b87d5f2fcb43a925b8ce2cb24bd455391
DTTEC\jmoren16 <jmoren16@tragsa.es>
2017-04-04 17:15:25 +0200

    1

--- www/js/controllers/sudokuResultCtrl.js
+++ www/js/controllers/sudokuResultCtrl.js
@@ -1,10 +1,23 @@
 appControllers.controller('sudokuResultCtrl', ['$scope', 'bridgeService', '$ionicSideMenuDelegate', 'sessionService', '$interval', '$rootScope',
     function ($scope, bridgeService, $ionicSideMenuDelegate, sessionService, $interval, $rootScope) {
+        var level = bridgeService.data.sudokuSelectLevel;
+        var aux = sessionService.get("progressSudoku");
+       
+        aux[level].board = null;
+        aux[level].wins = aux[level].wins + 1;
+        sessionService.set("progressSudoku", aux);
 
-        var level =  bridgeService.data.sudokuSelectLevel;
-        
+        $scope.level = dictionary[sessionService.get("config").lenguage][level];
+        $scope.time = millisToMinutesAndSeconds(aux[level].time);
+        $scope.totalSuccess = aux[level].wins;
 
         $scope.openSettings = function () {
             $ionicSideMenuDelegate.toggleLeft();
+        }
+
+        function millisToMinutesAndSeconds(millis) {
+            var minutes = Math.floor(millis / 60000);
+            var seconds = ((millis % 60000) / 1000).toFixed(0);
+            return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
         }
     }])
